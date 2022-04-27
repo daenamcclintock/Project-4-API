333cd79fbb304d61cf778b7e198951e8b39e7194
daenamcclintock <daena.mcclintock@gmail.com>
2022-04-27 11:29:40 -0400

    delete message working

--- REEL_ESTATE/app/routes/message_routes.js
+++ REEL_ESTATE/app/routes/message_routes.js
@@ -4,6 +4,7 @@ const passport = require('passport')
 const Property = require('../models/property')
 const customErrors = require('../../lib/custom_errors')
 const User = require('../models/user')
+const Message = require('../models/messages')
 
 const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
@@ -27,9 +28,12 @@ router.post('/messages/:propertyId', (req, res, next) => {
         .then(property => {
             console.log('this is the property', property)
             console.log('this is the message', message)
+            // find user by property owner id
             User.findById(property.owner._id)
                 .then(user => {
+                    // push the message to the user messages array
                     user.messages.push(message)
+                    // save the user data
                     return user.save()
                 })
         .then(user => res.status(201).json({ user: user }))
@@ -39,23 +43,32 @@ router.post('/messages/:propertyId', (req, res, next) => {
 
 // DELETE - Delete a message in the database
 // DELETE - /messages/propertyId/messageId
-router.delete('/messages/:propertyId/:messageId', requireToken, (req, res, next) => {
+router.delete('/messages/:userId/:messageId', requireToken, (req, res, next) => {
+    // get messageId from params
     const messageId = req.params.messageId
-    const propertyId = req.params.propertyId
+    console.log('this is messageId', messageId)
+    // get userId from params
+    const userId = req.params.userId
+    console.log('this is property id', userId)
 
-    Property.findById(propertyId)
+    User.findById(userId)
         // If property not found, throw 404
         .then(handle404)
-        .then(property => {
+        .then(user => {
             // get the specific subdocument by its id
-            const message = property.messages.id(messageId)
+            const messages = user.messages
+            console.log('this is messages', messages)
+            
             // require ownership of the message to delete
-            requireOwnership(req, property)
-            // remove the message
-            message.remove()
+            requireOwnership(req, messages)
 
-            // return the saved property
-            return property.save()
+            // find the index of the message by the id and splice it out of the array
+            const indexMessage = messages.map((message) => message._id).indexOf(messageId)
+            const removedMessage = indexMessage && messages.splice(indexMessage, 1)
+            console.log('removedMessage', removedMessage)
+            
+            // return the saved user
+            return user.save()
         })
         // if successful, send 204 status
         .then(() => res.sendStatus(204))
