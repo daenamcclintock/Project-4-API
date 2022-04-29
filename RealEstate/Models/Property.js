87d29a51635ca5e7c7024848d92a8416399e3f5b
daenamcclintock <daena.mcclintock@gmail.com>
2022-04-29 16:38:57 -0400

    create route working

--- RealEstate/Models/Property.js
+++ RealEstate/Models/Property.js
@@ -1,1 +1,21 @@
 const mongoose = require('mongoose')
+
+const PropertySchema = mongoose.Schema({
+    propertyAddress: {
+        address: {type: String, required: true},
+        city: {type: String, required: true},
+        state: {type: String, required: true}
+    },
+
+    propertyAttributes: {
+        price: {type: Number, required: true},
+        isSaleOrRent: {type: String, required: true},
+        numOfBedrooms: {type: String, required: true},
+        numOfBathrooms: {type: String, required: true},
+        numOfGarages: {type: String, required: true},
+
+    }
+
+})
+
+module.exports = mongoose.model('Property', PropertySchema)
--- RealEstate/server.js
+++ RealEstate/server.js
@@ -1,14 +1,34 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+require("dotenv").config();
 
-// ********** MIDDLEWARE **********
-app.use(cors())
+//MiddleWare
+app.use(cors());
 
-// ********** ROUTE CONSTANTS **********
+//API ENDPOINTS CONSTANTS
+const HouseListingRoute = require("./Routes/HouseUpload/HouseSaleOrRent");
+const ListHouseFecthRoute = require("./Routes/ListedHouseFetch/HouseFetch");
+const ContactFormRoute = require("./Routes/EmailRoute/Email");
+//API ENDPOINTS
+app.use(HouseListingRoute);
+app.use(ListHouseFecthRoute);
+app.use(ContactFormRoute);
 
-// ********** SERVER ENTRY **********
-const PORT = proccess.enc.PORT || 5000
+//Setting up for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (request, response) => {
+    response.sendFile(
+      path.resolve(__dirname, "./client", "build", "index.html")
+    );
+  });
+}
+
+//SERVER'S ENTRY POINT
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('Server started on port: ', PORT)
-})
+  console.log(`Server started on port ${PORT}`);
+});
